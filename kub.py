07753cce2f529a8b2fa27b1ecda7ad7d4ce5365b
niks <dniksdorf@gmail.com>
2018-06-04 23:26:59 +0300

    kub

---
+++ kub.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def cub():
+	i = 1
+	while True:
+		i += 1
+		cub1 = randint(1, 6)
+		cub2 = randint(1, 6)
+		if cub1 + cub2 == 8:
+			print(cub1, cub2, i)
+			break
+
+
+cub()
+
